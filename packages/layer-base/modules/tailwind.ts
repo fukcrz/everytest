db6115d6e56a06910988a33294f1798cecbfbf86
fukcrz <fszaxt@gmail.com>
2025-08-15 10:11:15 +0800

    feat(base): 每个 layer 的 `assets/css/index.css` 文件将自动导入

--- packages/layer-base/modules/tailwind.ts
+++ packages/layer-base/modules/tailwind.ts
@@ -1,29 +1,47 @@
-import { addTemplate, defineNuxtModule } from "nuxt/kit"
-import { relative } from "pathe"
+import { addTemplate, defineNuxtModule, resolveFiles } from "nuxt/kit"
+import { join, relative } from "pathe"
 
 export default defineNuxtModule({
     setup(_, nuxt) {
-        // addTemplate({
-        //     filename: "layer.css",
-        //     write: true,
-        //     async getContents() {
-        //         const buildDir = nuxt.options.buildDir
-        //         const dirs = nuxt.options._layers.map((layer) => relative(buildDir, layer.config.rootDir))
-        //         return dirs.map((dir) => `@source "${dir}";`).join("\n")
-        //     },
-        // })
         addTemplate({
             filename: "tailwind.css",
             write: true,
             async getContents() {
-                const buildDir = nuxt.options.buildDir
-                const dirs = nuxt.options._layers.map((layer) =>
-                    relative(buildDir, layer.config.rootDir),
+                const appDirs = nuxt.options._layers.map(
+                    (layer) =>
+                        layer.config.appDir ?? layer.config.rootDir + "/app",
                 )
-                const source = dirs.map((dir) => `@source "${dir}/app";`).join("\n")
+
+                const buildDir = nuxt.options.buildDir
+                const source = appDirs
+                    .map((dir) => relative(buildDir, dir))
+                    .map((dir) => `@source "${dir}";`)
+                    .join("\n")
+
+                const importFiles: string[] = []
+                for (const dir of appDirs) {
+                    const cssFiles = await resolveFiles(
+                        join(dir, "assets/css"),
+                        "index.css",
+                    )
+                    if (cssFiles.length > 0) {
+                        importFiles.push(
+                            relative(
+                                buildDir,
+                                join(dir, "assets/css/index.css"),
+                            ),
+                        )
+                    }
+                }
+                const imports = importFiles
+                    .map((item) => `@import "${item}";`)
+                    .join("\n")
+
                 return (
                     '@import "tailwindcss";\n' +
                     '@import "@nuxt/ui";\n' +
+                    imports +
+                    "\n" +
                     source
                 )
             },
